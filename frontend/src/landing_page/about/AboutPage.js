9337664a2f670dfa5ee56c8d706d8303fb58aa8e
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-16 12:30:41 +0530

    fifth

--- frontend/src/landing_page/about/AboutPage.js
+++ frontend/src/landing_page/about/AboutPage.js
@@ -1,18 +1,15 @@
 import React from "react";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 import Hero from "./Hero";
 import Team from "./Team";
 
-function HomePage() {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <Team />
-            <Footer />
-        </>
-    );
+function AboutPage() {
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
 }
 
 export default AboutPage;
--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,18 +1,15 @@
 import React from "react";
-import Navbar from "../Navbar";
-import Footer from "../Footer";
+
 import Hero from "./Hero";
 import Brokerage from "./Brokerage";
 
-function HomePage() {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <Brokerage />
-            <Footer />
-        </>
-    );
+function PricingPage() {
+  return (
+    <>
+      <Hero />
+      <Brokerage />
+    </>
+  );
 }
 
 export default PricingPage;
