1772138f51e4a9501d0535a373643adbfddb72bf
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-21 23:24:09 +0530

    op

---
+++ frontend/src/landing_page/support/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket, select a relevent topic</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
 
 function SupportPage() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
 export default SupportPage;
