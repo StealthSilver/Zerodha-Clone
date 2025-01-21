f014e8c5e9add860b71790cfa0117aa4b22aa2c5
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2025-01-21 23:39:56 +0530

    styles

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -2,11 +2,12 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="container">
-      <div className="row p-5 mt-5 mb-5">
-        <h1 className="fs-2">To create a ticket, select a relevent topic</h1>
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 mt-5 mb-5" id="supportWrapper">
+        <h3>Support Portal</h3>
+        <a href="">Track Tickets</a>
       </div>
-    </div>
+    </section>
   );
 }
 
